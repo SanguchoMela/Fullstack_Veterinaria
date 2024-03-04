534ecdb1f61068a73f6a13070eddaf5065614228
SanguchoMela <melany01-03@hotmail.com>
2024-03-04 12:48:05 -0500

    update modulo tratamientos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,41 +16,44 @@ import { Confirmar } from './paginas/Confirmar'
 import Restablecer from './paginas/Restablecer'
 import { AuthProvider } from './context/AuthProvider'
 import { PrivateRoute } from './routes/PrivateRoute'
+import { TratamientosProvider } from './context/TratamientosProvider'
 
 function App() {
   return (
     <>
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          
-          {/* <Route index element={<LandinPage/>}/> */}
+        <TratamientosProvider>
+          <Routes>
+            
+            {/* <Route index element={<LandinPage/>}/> */}
 
-          <Route path='/' element={<Auth/>}>
-            <Route index element={<LandinPage/>}/>
-            <Route path='login' element={<Login/>}/>
-            <Route path='register' element={<Register/>}/>
-            <Route path='forgot/:id' element={<Forgot/>}/>
-            <Route path='confirmar/:token' element={<Confirmar/>}/>
-            <Route path='recuperar-password/:token' element={<Restablecer/>}/>
-            <Route path='*' element={<NotFound />} />
-          </Route>
+            <Route path='/' element={<Auth/>}>
+              <Route index element={<LandinPage/>}/>
+              <Route path='login' element={<Login/>}/>
+              <Route path='register' element={<Register/>}/>
+              <Route path='forgot/:id' element={<Forgot/>}/>
+              <Route path='confirmar/:token' element={<Confirmar/>}/>
+              <Route path='recuperar-password/:token' element={<Restablecer/>}/>
+              <Route path='*' element={<NotFound />} />
+            </Route>
 
-          <Route path='dashboard/*' element={
-            <PrivateRoute>
-              <Routes>
-                <Route element={<Dashboard />}>
-                  <Route index element={<Perfil />} />
-                  <Route path='listar' element={<Listar />} />
-                  <Route path='visualizar/:id' element={<Visualizar />} />
-                  <Route path='crear' element={<Crear />} />
-                  <Route path='actualizar/:id' element={<Actualizar />} />
-                </Route>
-              </Routes>
-            </PrivateRoute>
-          } />
+            <Route path='dashboard/*' element={
+              <PrivateRoute>
+                <Routes>
+                  <Route element={<Dashboard />}>
+                    <Route index element={<Perfil />} />
+                    <Route path='listar' element={<Listar />} />
+                    <Route path='visualizar/:id' element={<Visualizar />} />
+                    <Route path='crear' element={<Crear />} />
+                    <Route path='actualizar/:id' element={<Actualizar />} />
+                  </Route>
+                </Routes>
+              </PrivateRoute>
+            } />
 
-        </Routes>
+          </Routes>
+        </TratamientosProvider>
       </AuthProvider>
     </BrowserRouter>
     </>
